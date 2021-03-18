8eb730dcb47e7bdeb16d4c42438b99fecc1e2d23
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-18 17:39:21 -0400

    commit tree 307f7c634c4acdfd78d552305aba72cc896277c6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-    let textElement = document.querySelector("#text")
-    textElement.innerHTML = "This is really cool!"
+    // let textElement = document.querySelector("#text")
+    // textElement.innerHTML = "This is really cool!"
+    document.querySelector("#text").innerHTML = "This is really cool!"
 })
