03be5261f215a94d6462fc71a18a8862118ce1c1
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-18 17:36:35 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let textElement = document.querySelector("#text")
+    textElement.innerHTML = "This is really cool!"
+})
